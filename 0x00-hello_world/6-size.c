003e3cc3092dfa30080f0c1c6f28e676d5e604f9
basmala77 <basmalaelshabrawy37@gmail.com>
2023-03-16 21:49:37 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Prints the size of various types  the computer it is compiled and run on..
+ * main - Prints the size of various types  the computer it is compiled and run
  * Return: Always 0
  */
 int main(void)
